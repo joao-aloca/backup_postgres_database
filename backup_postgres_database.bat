ca600a343390afcc637581f47f6a4347a6df0c10
João Alóca <125145954+joao-aloca@users.noreply.github.com>
2025-09-17 14:35:02 -0300

    Adicionando verificação do caminho
    Condicional com o intuito de verificar se o caminho onde o arquivo de backup será criado já existe

--- backup_postgres_database.bat
+++ backup_postgres_database.bat
@@ -1,45 +1,60 @@
-:: Criado em: 08/09/2025
-:: Autor: João Alóca
-:: Email: joao.aloca@outlook.com
-
-
 @echo off
 
-:: Dados de conexão
-set "host=[hostname]"
-set "port=[porta]"
-set "user=[usuario]"
-set "database=[nome_do_banco_de_dados]"
+:: DECLARAÇÃO E INICIALIZAÇÃO DAS VARIÁVEIS
 
-:: Nome do arquivo de saída
-set "nome_arquivo=%database%"
+    ::CONEXÃO COM O BANCO DE DADOS
 
-:: Variáveis para a data e hora
-set "data_completa=%date%"
-set "hora_completa=%time%"
+        :: Dados de conexão com o servidor
+        set "host=[ip_host]"
+        set "port=[porta]"
+        set "user=[user]"
 
-:: Variáveis para o dia, mês e ano
-set "ano=%data_completa:~-4%"
-set "mes=%data_completa:~3,2%"
-set "dia=%data_completa:~0,2%"
+        :: Nome do banco de dados
+        set "database=[nome_banco]"
+    ::
 
-:: Variáveis para as horas e minutos
-set "horas=%hora_completa:~0,2%"
-set "minutos=%hora_completa:~3,2%"
+    :: DATA E HORA
 
-:: Variável para a mensagem de finalização
-set "mensagem_sucesso=Backup finalizado com sucesso!"
+        :: Data
+        set "ano=%date:~-4%"
+        set "mes=%date:~3,2%"
+        set "dia=%date:~0,2%"
 
-:: Criação dos diretórios para separação dos arquivos de backup por ano e competência
-mkdir .\%ano%\%mes%-%ano%\
+        :: Horas
+        set "horas=%time:~0,2%"
+        set "minutos=%time:~3,2%"
+    ::
 
-:: Geração do arquivo de backup via pg_dump no formato plain-text
-pg_dump -h %host% -p %port% -U %user% -d %database% -w -v -F p -f .\%ano%\%mes%-%ano%\%nome_arquivo%_%dia%%mes%%ano%_%horas%%minutos%.sql
+    :: ARQUIVO DE BACKUP
 
-:: Geração do arquivo de backup via pg_dump no formato custom
-pg_dump -h %host% -p %port% -U %user% -d %database% -w -v -F c -f .\%ano%\%mes%-%ano%\%nome_arquivo%_%dia%%mes%%ano%_%horas%%minutos%.dmp
+        :: Caminho do arquivo
+        set "backup_path=.\%ano%\%mes%.%ano%"
 
-:: Exibição da mensagem de término do backup
-echo %mensagem_sucesso%
+        :: Nome do arquivo
+        set "backup_file_name=%database%_%dia%%mes%%ano%_%horas%%minutos%"
+    ::
 
-:: pause
+::
+
+:: CRIAÇÃO DA ESTRUTURA DE DIRETÓRIOS
+    
+    :: Verificando se a estrutura de diretórios já existe
+    if not exist %backup_path% (
+        mkdir %backup_path%
+    )
+::
+
+
+:: EXECUÇÃO DO PG_DUMP
+    
+	:: Geração do arquivo de backup no formato plain-text
+    @REM pg_dump -h %host% -p %port% -U %user% -d %database% -w -v -F p -f "%backup_path%\%backup_file_name%.sql"
+
+    :: Geração do arquivo de backup no formato custom
+    @REM pg_dump -h %host% -p %port% -U %user% -d %database% -w -v -F c -f "%backup_path%\%backup_file_name%.dmp"
+
+    :: Geração do arquivo de backup no formato custom com extensão .backup
+	pg_dump -h %host% -p %port% -U %user% -d %database% -w -v -F c -f "%backup_path%\%backup_file_name%.backup"
+::
+
+pause
